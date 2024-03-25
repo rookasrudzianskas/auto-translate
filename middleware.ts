6c3f77f4788aa97ef3c0ea04374a124930eaf898
Rokas <rookas.rudzenskas@gmail.com>
2024-03-25 13:10:53 +0200

    fix

--- middleware.ts
+++ middleware.ts
@@ -11,5 +11,5 @@ export default clerkMiddleware((auth, req) => {
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/translate", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/translate", "/transcribeAudio", "/translationHistory", "/(api|trpc)(.*)"],
 };
