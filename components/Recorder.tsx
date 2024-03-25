eaa6b9e37135094ad868443f0b019035b19032a5
Rokas <rookas.rudzenskas@gmail.com>
2024-03-25 12:00:54 +0200

    fix

--- components/Recorder.tsx
+++ components/Recorder.tsx
@@ -35,6 +35,40 @@ const Recorder = ({ uploadAudio }: { uploadAudio: (blob: Blob) => void }) => {
     }
   };
 
+  const startRecording = async () => {
+    if (stream === null || pending) return;
+
+    setRecordingStatus("recording");
+
+    // Create a new media recorder instance using the stream
+    const media = new MediaRecorder(stream, { mimeType });
+    mediaRecorder.current = media;
+    mediaRecorder.current.start();
+
+    let localAudioChunks: Blob[] = [];
+
+    mediaRecorder.current.ondataavailable = (event) => {
+      if (typeof event.data === "undefined") return;
+      if (event.data.size === 0) return;
+
+      localAudioChunks.push(event.data);
+    };
+
+    setAudioChunks(localAudioChunks);
+  };
+
+  const stopRecording = async () => {
+    if (mediaRecorder.current === null || pending) return;
+
+    setRecordingStatus("inactive");
+    mediaRecorder.current.stop();
+    mediaRecorder.current.onstop = () => {
+      const audioBlob = new Blob(audioChunks, { type: mimeType });
+      uploadAudio(audioBlob);
+      setAudioChunks([]);
+    };
+  };
+
   return (
     <div
       className={`flex items-center group text-blue-500 cursor-pointer border rounded-md w-fit px-3 py-2  mb-5 ${
