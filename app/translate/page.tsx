2dc3221ebf76825df91f068ab206c33807343d56
Rokas <rookas.rudzenskas@gmail.com>
2024-03-25 09:50:46 +0200

    Fix

--- app/translate/page.tsx
+++ app/translate/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
+
+export type TranslationLanguages = {
+  translation: {
+    [key: string]: {
+      name: string;
+      nativeName: string;
+      dir: "ltr" | "rtl";
+    };
+  };
+};
 
 const TranslatePage = ({}) => {
+  auth().protect();
+
+  const { userId } = auth();
+  if (!userId) throw new Error("User not logged in");
+
   return (
     <div>
-      
+
     </div>
   );
 };
