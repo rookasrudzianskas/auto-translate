1d6d64cad8e4c9f5763378264c7285dd6b47deab
Rokas <rookas.rudzenskas@gmail.com>
2024-03-25 12:31:44 +0200

    end of open ai

--- app/transcribeAudio/route.ts
+++ app/transcribeAudio/route.ts
@@ -28,4 +28,25 @@ export async function POST(request: NextRequest) {
   }
 
   console.log(">>", file);
+
+  const arrayBuffer = await file.arrayBuffer();
+  const audio = new Uint8Array(arrayBuffer);
+
+  // ---   get audio transcription from Azure Whisper AI service ----
+
+  console.log("== Transcribe Audio Sample ==");
+
+  const client = new OpenAIClient(
+    process.env.AZURE_ENDPOINT,
+    new AzureKeyCredential(process.env.AZURE_API_KEY)
+  );
+
+  const result = await client.getAudioTranscription(
+    process.env.AZURE_DEPLOYMENT_NAME,
+    audio
+  );
+
+  console.log(`Transcription: ${result.text}`);
+
+  return NextResponse.json({ text: result.text });
 }
