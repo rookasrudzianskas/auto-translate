76e054657ade093b3dcf72b1763fff6f59bf1ced
Rokas <rookas.rudzenskas@gmail.com>
2024-03-25 11:25:09 +0200

    history and mongooose setup

--- components/TranslationHistory.tsx
+++ components/TranslationHistory.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import { ITranslation } from "@/mongodb/models/User";
+import { auth } from "@clerk/nextjs/server";
+import DeleteTranslationButton from "./DeleteTranslationButton";
+import TimeAgo from "react-timeago";
+import TimeAgoText from "./TimeAgoText";
 
 const TranslationHistory = ({}) => {
   return (
     <div>
-      
+
     </div>
   );
 };
